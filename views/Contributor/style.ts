f4d44907281c32b782a4af87216bf3a743ffaabe
guoqqqi <979918879@qq.com>
2021-03-11 11:26:07 +0800

    fix: update codes

--- views/Contributor/style.ts
+++ views/Contributor/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SWrapper = styled.div`
   iframe {
     width: 100%;
-    min-height: 870px;
+    min-height: 890px;
     border: none;
     margin-top: 40px;
   }
@@ -11,6 +11,12 @@ export const SWrapper = styled.div`
   @media (max-width: 480px) {
     iframe {
       min-height: 920px !important;
+    }
+  }
+
+  @media (max-width: 350px) {
+    iframe {
+      min-height: 1000px !important;
     }
   }
 `
