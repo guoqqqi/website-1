4d89ec784287cb49655c2b65e163b3840faff446
guoqqqi <979918879@qq.com>
2021-03-23 14:56:01 +0800

    fix: home mobile style

--- views/Home/components/HomeFeature3/index.tsx
+++ views/Home/components/HomeFeature3/index.tsx
@@ -21,7 +21,7 @@ const App: NextPage<Props, any> = ({ t }) => {
     <Box as="section" bg={mode('gray.100', 'gray.800')} pb="20">
       <Box maxW={{ base: 'xl', md: '7xl' }} mx="auto" px={{ base: '6', md: '8' }}>
         <Stack spacing={{ base: '4', lg: '20' }} direction={{ base: 'column-reverse', lg: 'row' }}>
-          <Center flex="1" shadow="lg" minH="26rem" maxW={{ lg: 'xl' }}>
+          <Center flex="1" shadow="lg" minH={[0, 420]} maxW={{ lg: 'xl' }}>
             <Img
               objectFit="contain"
               w="full"
